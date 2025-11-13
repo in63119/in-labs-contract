e9f3a95c2f78f21f42fbef6ec8e0495f3bd28412
fat-boy <fat-boy@jeonghyeonjin-ui-iMac.local>
2025-11-13 11:32:19 +0900

    feat: add func delete, update

--- contracts/YoutubeStorage.sol
+++ contracts/YoutubeStorage.sol
@@ -12,10 +12,13 @@ contract YoutubeStorage is Ownable {
 
     error YoutubeStorage__InvalidAccount();
     error YoutubeStorage__IndexOutOfBounds();
+    error YoutubeStorage__VideoNotFound();
 
     mapping(address => YouTubeVideo[]) private _videosByAccount;
 
     event VideoStored(address indexed account, string indexed id, string title);
+    event VideoDeleted(address indexed account, string indexed id);
+    event VideoUpdated(address indexed account, string indexed id, string title);
 
     constructor() Ownable(msg.sender) {}
 
@@ -35,6 +38,31 @@ contract YoutubeStorage is Ownable {
         string[] calldata keyPoints
     ) external onlyOwner {
         _storeVideo(account, videoId, title, keyPoints);
+    }
+
+    function updateMyVideo(
+        string calldata videoId,
+        string calldata title,
+        string[] calldata keyPoints
+    ) external {
+        _updateVideo(msg.sender, videoId, title, keyPoints);
+    }
+
+    function updateVideoFor(
+        address account,
+        string calldata videoId,
+        string calldata title,
+        string[] calldata keyPoints
+    ) external onlyOwner {
+        _updateVideo(account, videoId, title, keyPoints);
+    }
+
+    function deleteMyVideo(string calldata videoId) external {
+        _deleteVideoById(msg.sender, videoId);
+    }
+
+    function deleteVideoFor(address account, string calldata videoId) external onlyOwner {
+        _deleteVideoById(account, videoId);
     }
 
     /* ========== Getter Functions ========== */
@@ -76,6 +104,54 @@ contract YoutubeStorage is Ownable {
         }
 
         emit VideoStored(account, videoId, title);
+    }
+
+    function _updateVideo(
+        address account,
+        string calldata videoId,
+        string calldata title,
+        string[] calldata keyPoints
+    ) private {
+        if (account == address(0)) revert YoutubeStorage__InvalidAccount();
+
+        YouTubeVideo[] storage videos = _videosByAccount[account];
+        for (uint256 i = 0; i < videos.length; i++) {
+            if (keccak256(bytes(videos[i].id)) == keccak256(bytes(videoId))) {
+                YouTubeVideo storage videoToUpdate = videos[i];
+                videoToUpdate.title = title;
+
+                delete videoToUpdate.keyPoints;
+                for (uint256 j = 0; j < keyPoints.length; j++) {
+                    videoToUpdate.keyPoints.push(keyPoints[j]);
+                }
+
+                emit VideoUpdated(account, videoId, title);
+                return;
+            }
+        }
+
+        revert YoutubeStorage__VideoNotFound();
+    }
+
+    function _deleteVideoById(address account, string calldata videoId) private {
+        if (account == address(0)) revert YoutubeStorage__InvalidAccount();
+
+        YouTubeVideo[] storage videos = _videosByAccount[account];
+        uint256 length = videos.length;
+
+        for (uint256 i = 0; i < length; i++) {
+            if (keccak256(bytes(videos[i].id)) == keccak256(bytes(videoId))) {
+                uint256 lastIndex = length - 1;
+                if (i != lastIndex) {
+                    videos[i] = videos[lastIndex];
+                }
+                videos.pop();
+                emit VideoDeleted(account, videoId);
+                return;
+            }
+        }
+
+        revert YoutubeStorage__VideoNotFound();
     }
 
     function _copyVideoToMemory(YouTubeVideo storage videoData) private view returns (YouTubeVideo memory) {
